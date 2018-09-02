9f34c3950885df254d24cfd717f610baf0c077fd
Karen Posada <k100posada@gmail.com>
2018-09-02 14:38:33 -0500

    explicit vs implicit

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
 //Variable types
 
+//EXPLICIT TYPES
+
 //lets assign example one to a boolean
 let example1: boolean = true;
 
 let example2: number = 35;
 
+let example2a: number;
+
 let example3: string = 'Hello world';
 
 //multiple variables but single types are better
-let example1: boolean || number = true;
+let example1a: boolean || number = true;
+
+//subtypes if you dont know what the variable will be
+
+let example4: number = undefined;
+
+let example5: undefined = undefined;
+
+let example6: null = undefined;
+
+const arrayExample: number[] = [1, 2, 3, 4];
+
+let example7 = arrayExample.reduce((num1, num2) => num1 + num2);
