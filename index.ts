3fd51632e4a210a6233135e076f23c44e943078f
Karen Posada <k100posada@gmail.com>
2018-09-02 14:30:53 -0500

    Adding multiple variables

--- index.ts
+++ index.ts
@@ -6,3 +6,6 @@ let example1: boolean = true;
 let example2: number = 35;
 
 let example3: string = 'Hello world';
+
+//multiple variables but single types are better
+let example1: boolean || number = true;
